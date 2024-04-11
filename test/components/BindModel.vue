2fb8e9b4602c130c8a4b0ad2898293fad332899d
Alexander Shabunevich <loonar@gmail.com>
2024-04-11 22:41:55 +0300

    test: add BindModel component

---
+++ test/components/BindModel.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { vMaska } from '../../src'
+
+const value = ref('123')
+const bound = ref('')
+</script>
+
+<template>
+  <input v-maska:bound.unmasked data-maska="#-#" data-maska-eager v-model="value" />
+  <div id="value1">{{ value }}</div>
+  <div id="value2">{{ bound }}</div>
+</template>
