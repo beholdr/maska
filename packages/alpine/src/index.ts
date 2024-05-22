f6c706f1c884f971601a9f8434f254db47fa45c3
Alexander Shabunevich <loonar@gmail.com>
2024-05-22 21:21:41 +0300

    feat: auto init for alpine

--- packages/alpine/src/index.ts
+++ packages/alpine/src/index.ts
@@ -12,3 +12,10 @@ import { xMaska } from './alpine'
 
 export { Mask, MaskInput, tokens, xMaska }
 export type { MaskaDetail, MaskInputOptions, MaskOptions, MaskTokens, MaskType }
+
+if (document.currentScript?.dataset.init !== undefined) {
+  document.addEventListener('alpine:init', () => {
+    // @ts-expect-error
+    window.Alpine.plugin(xMaska)
+  })
+}
